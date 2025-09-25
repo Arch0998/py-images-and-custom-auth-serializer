4ec72c5fc5bc2ec2da03e63f79e44f50cbd313de
Arthur Oleinikov <arch0998@gmail.com>
2025-09-25 15:38:15 +0300

    Fix serializer validation

--- cinema/views.py
+++ cinema/views.py
@@ -112,7 +112,7 @@ class MovieViewSet(
         serializer = ImageUploadSerializer(
             movie, data=request.data, partial=True
         )
-        if serializer.is_valid(raise_exceptions=True):
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=HTTP_200_OK)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
